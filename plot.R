32064ac03515b14a4fa0e22d79efcd844b3e8c02
jesse <jesse>
2018-04-20 11:09:05 -0400

    Add plot to bin per decade

--- plot.R
+++ plot.R
@@ -5,9 +5,26 @@ xs <- vector('numeric', length(years))
 agg <- aggregate(d$YEAR, by=list(YEAR=d$YEAR), FUN=length)
 idx <- match(agg$YEAR, years)
 xs[idx] <- agg$x
-fname <- sprintf("results/%s-radio1045.pdf", format(Sys.time (), "%Y-%m-%d"))
+prefix <- format(Sys.time (), "%Y-%m-%d")
+
+fname <- sprintf("results/%s-year-radio1045.pdf", prefix)
 pdf(file=fname, width=11, height=8.5)
 barplot(xs, names.arg=years, las=2,
         main=sprintf("Radio 1045 Artist/Song by Year (%d samples)\nCollected: %s", 
                      length(d$SONG), date()))
 dev.off ()
+
+# the 90s should precede 2000+ so we need the century for sorting purposes
+d$CENT <- d$YEAR %/% 100
+d$DECADE <- ((d$YEAR %% 100) %/% 10) * 10
+agg <- aggregate(d$DECADE, by=list(DECADE=d$DECADE, CENT=d$CENT), FUN=length)
+agg <- agg[order(agg$CENT, agg$DECADE),]
+
+fname <- sprintf("results/%s-decade-radio1045.pdf", prefix)
+pdf(file=fname, width=11, height=8.5)
+barplot(agg$x, names.arg=sprintf("%02ds", agg$DECADE),
+        main=sprintf("Radio 1045 Artist/Song by Decade (%d samples)\nCollected: %s", 
+                     length(d$SONG), date()))
+dev.off ()
+
+
